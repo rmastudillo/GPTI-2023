33e5295d6f2c46d89d1b5c3aa2b3e572bb87452f
Richard Astudillo <richard@skipo.io>
2023-05-14 18:51:11 -0400

    backend conectado

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,26 +1,9 @@
 import { API_HOST } from "@/utils/constants";
-import axios, {
-  AxiosRequestTransformer,
-  AxiosResponseTransformer
-} from "axios";
-import { camelizeKeys, decamelizeKeys } from "humps";
+import axios from "axios";
 
 export const client = axios.create({
   baseURL: API_HOST,
   headers: {
     "Content-Type": "application/json",
   },
-  transformResponse: [
-    ...(axios.defaults.transformResponse as AxiosResponseTransformer[]),
-    (data) => camelizeKeys(data),
-  ],
-  transformRequest: [
-    (data) => decamelizeKeys(data),
-    ...(axios.defaults.transformRequest as AxiosRequestTransformer[]),
-  ],
-});
-
-client.interceptors.request.use((config) => {
-  const { params, ...noParamsConfig } = config;
-  return { ...noParamsConfig, params: decamelizeKeys(params) };
 });
