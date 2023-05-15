1a529082ad3929fccca02f2de191edc133d0f2b4
Richard Astudillo <richard@skipo.io>
2023-05-14 20:47:20 -0400

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [vue()],
-  base: 'rmastudillo.github.io/GPTI-2023/',
+  base: 'https://rmastudillo.github.io/GPTI-2023/',
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
