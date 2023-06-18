36f812f211a3d6f0ec5c46193fe95cd2266e7c11
Richard Astudillo <richard@skipo.io>
2023-06-18 14:15:40 -0400

    buscador listo

--- src/api/modules/default.ts
+++ src/api/modules/default.ts
@@ -3,3 +3,6 @@ import { client } from "@/api/client";
 export const getProducts = () => client.get("/products");
 
 export const getAllCategories = () => client.get("/prueba");
+
+export const getFilteredProducts = (params: any = {}) =>
+  client.get("/buscar", { params });
